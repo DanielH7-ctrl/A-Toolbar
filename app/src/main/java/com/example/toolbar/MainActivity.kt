2dc1c56528cf361bd6d77070d65a28d5753c5b9f
DanielH7-ctrl <danielhuerta826@gmail.com>
2022-03-06 21:34:16 -0500

    Compartir contenido

--- app/src/main/java/com/example/toolbar/MainActivity.kt
+++ app/src/main/java/com/example/toolbar/MainActivity.kt
@@ -7,9 +7,11 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
-import android.widget.SearchView
+import androidx.appcompat.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.widget.ShareActionProvider
+import androidx.core.view.MenuItemCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -37,6 +39,9 @@ class MainActivity : AppCompatActivity() {
 
         val itemBusqueda = menu?.findItem(R.id.busqueda)
         val vistaBusqueda = itemBusqueda?.actionView as SearchView
+        val itemCompartir = menu?.findItem(R.id.share)
+        val shareActionProvider = MenuItemCompat.getActionProvider(itemCompartir) as ShareActionProvider
+        compartirIntent(shareActionProvider)
 
         vistaBusqueda.queryHint = "Escribe tu nombre..."
 
@@ -68,6 +73,14 @@ class MainActivity : AppCompatActivity() {
             }
             else ->(return super.onOptionsItemSelected(item))
 
+        }
+    }
+    private fun compartirIntent(shareActionProvider: ShareActionProvider) {
+        if (shareActionProvider != null) {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, "Este es un mensaje compartido")
+            shareActionProvider.setShareIntent(intent)
         }
     }
 }
